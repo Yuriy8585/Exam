ba0ea12e19e0be68982014c4545e569859135775
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2024-06-18 12:19:31 +0300

    commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,37 +1,14 @@
-import { useState, useEffect } from 'react';
+import GistsList from './items/GistsList';
 
 
 function Cart() {
-  const [cartItems, setCartItems] = useState([]);
-
-  async function fetchProducts() {
-    const response = await fetch('https://example.com/api/products');
-    const products = await response.json();
-    return products;
-  }
-
-  function AddToCart(item) {
-    setCartItems([...cartItems, item]);
-    
-  }
   
-
-  useEffect(() => {
-    fetchProducts().then(products => {
-      const cartItems = products.filter(product => product.inCart);
-      setCartItems(cartItems);
-    });
-  }, []);
 
   return (
     <div>
-      <h1>Shop box</h1>
-      <br></br>
-      <ul>
-        {cartItems.map(item => (
-          <li key={item.id}>{item.name} - {item.price}</li>
-        ))}
-      </ul>
+      
+      <GistsList/>
+      
     </div>
   );
 }
---
+++ src/components/items/GistsList.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import CircularProgress from "@mui/material/CircularProgress";
+
+export const API_URL_PUBLIC = "https://api.github.com/gists/public";
+
+export const GistsList = () => {
+    const [gists, setGists] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const requestGists = async () => {
+        setLoading(true);
+        try {
+        const response = await fetch(API_URL_PUBLIC);
+        if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+        }
+        const result = await response.json();
+        setGists(result);
+        } catch (err) {
+        setError(true);
+        console.warn(err);
+        } finally {
+        setLoading(false);
+        }
+        };
+
+
+useEffect(() => {
+    requestGists();
+}, []);
+
+const renderGist = useCallback(
+(gist) => <li key={gist.id}>{gist.description}</li>,
+[]
+);
+
+    if (loading) { 
+        return <CircularProgress />;
+    }
+    
+    if (error) { return (
+    <>
+        <h3>Error</h3>
+        <button onClick={requestGists}>Retry</button>
+    </> );}
+
+    if (gists.lenght === 0) {return (<p>No gists</p>);}
+return (
+    
+    <div>
+        <h1>Shop box</h1>
+        <br></br>
+        <ul>{gists.map(renderGist)}</ul>
+        <h1>{gists.id}<p className="product_name"></p></h1>
+        <h2>{gists.description}<p className="product-description"></p></h2>
+        
+        <button onClick={renderGist}>Добавить в корзину</button>
+    </div>
+);
+
+}
+
+export default GistsList;
